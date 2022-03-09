837fb88322ccfe2ab6034fe062f0aa3ddf61b449
Joseph Shaw <j.g.shaw95@gmail.com>
2022-03-09 09:51:03 +0000

    Add caching and different timer page

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,38 @@
-self.addEventListener('install', function() {
-  console.log('Install!');
+const cacheName = 'lap-timer-v1';
+const appFiles = [
+  'index.html',
+  'timer.html',
+  'images/icon512.png',
+  'images/icon192.png',
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(appFiles);
+  })());
 });
-self.addEventListener("activate", event => {
-  console.log('Activate!');
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
-self.addEventListener('fetch', function(event) {
-  console.log('Fetch!', event.request);
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }))
+  }));
 });
